957c1cfb3ec31c9efb2ff7d93119c4b675cf0f0a
olesyakirillova <olesya.kiril2014@gmail.com>
2024-09-07 21:26:44 +0700

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        System.out.println(inc(number));
+    }
+
+    public static int inc(int number) {
+        return number + 1;
+    }
+}
